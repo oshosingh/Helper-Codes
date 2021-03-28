f2951eb86374aa78ee77916fa1eb1558cee34c2e
osho <oshosingh25@gmail.com>
2021-03-28 13:05:58 +0530

    added delete method

--- ds/BalancedTree.py
+++ ds/BalancedTree.py
@@ -91,18 +91,71 @@ class AVL:
         self.preOrder(root.left)
         self.preOrder(root.right)
 
-myTree = AVL()
-root = None
- 
-root = myTree.insert(root, 10)
-root = myTree.insert(root, 20)
-root = myTree.insert(root, 30)
-root = myTree.insert(root, 40)
-root = myTree.insert(root, 50)
-root = myTree.insert(root, 25)
-
-print("Preorder traversal of the",
-      "constructed AVL tree is")
-myTree.preOrder(root)
-print()
+    
+    # Recursive function to delete a node with
+    # given key from subtree with given root.
+    # It returns root of the modified subtree.
+    def delete(self, root, key):
+  
+        # Step 1 - Perform standard BST delete
+        if not root:
+            return root
+  
+        elif key < root.val:
+            root.left = self.delete(root.left, key)
+  
+        elif key > root.val:
+            root.right = self.delete(root.right, key)
+  
+        else:
+            if root.left is None:
+                temp = root.right
+                root = None
+                return temp
+  
+            elif root.right is None:
+                temp = root.left
+                root = None
+                return temp
+  
+            temp = self.getMinValueNode(root.right)
+            root.val = temp.val
+            root.right = self.delete(root.right,
+                                      temp.val)
+  
+        # If the tree has only one node,
+        # simply return it
+        if root is None:
+            return root
+  
+        # Step 2 - Update the height of the 
+        # ancestor node
+        root.height = 1 + max(self.getHeight(root.left),
+                            self.getHeight(root.right))
+  
+        # Step 3 - Get the balance factor
+        balance = self.getBalance(root)
+  
+        # Step 4 - If the node is unbalanced, 
+        # then try out the 4 cases
+        # Case 1 - Left Left
+        if balance > 1 and self.getBalance(root.left) >= 0:
+            return self.rightRotate(root)
+  
+        # Case 2 - Right Right
+        if balance < -1 and self.getBalance(root.right) <= 0:
+            return self.leftRotate(root)
+  
+        # Case 3 - Left Right
+        if balance > 1 and self.getBalance(root.left) < 0:
+            root.left = self.leftRotate(root.left)
+            return self.rightRotate(root)
+  
+        # Case 4 - Right Left
+        if balance < -1 and self.getBalance(root.right) > 0:
+            root.right = self.rightRotate(root.right)
+            return self.leftRotate(root)
+  
+        return root
+    
         
